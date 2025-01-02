b85ddb7ea3792cb2e41277b06972676526993054
Allan Lykke Christensen <allan.lykke.christensen@gmail.com>
2025-01-02 22:21:24 +0100

    ci: disable plugin validation to ensure SwiftLint runs in CI
    Disabled plugin validation in the CI configuration to ensure SwiftLint is enabled and runs during the build process. This guarantees consistent linting and adherence to code quality standards.

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,6 +5,8 @@
 //  Created by Allan Lykke Christensen on 02/01/2025.
 //
 
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+
 brew install swiftlint
 
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool
